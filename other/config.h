e5f51391266d7c090b96b581b889d872da000c14
Toni Jarjour <39845673+tonijarjour@users.noreply.github.com>
2021-08-02 08:02:10 -0400

    dwm border color change

--- other/config.h
+++ other/config.h
@@ -12,7 +12,7 @@ static const char col_gray2[]       = "#444444";
 static const char col_gray3[]       = "#bbbbbb";
 static const char col_gray4[]       = "#eeeeee";
 static const char col_blue[]        = "#244078";
-static const char col_red[]         = "#e40066";
+static const char col_red[]         = "#d81159";
 static const char *colors[][3]      = {
 	/*               fg         bg         border   */
 	[SchemeNorm] = { col_gray3, col_gray1, col_gray2 },
