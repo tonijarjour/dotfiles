ae471d94627620e792acf7f9935a88307026227d
Toni Jarjour <39845673+tonijarjour@users.noreply.github.com>
2021-11-11 15:17:39 -0500

    tabbed autohide and move dmenu to pacman

--- install.sh
+++ install.sh
@@ -9,7 +9,7 @@ doas pacman -S git gcc patch make neovim man-db fd ripgrep \
     noto-fonts noto-fonts-cjk noto-fonts-emoji ttf-liberation \
     ttf-iosevka-nerd chromium alsa-utils pkgstats feh mpv nnn \
     zathura-pdf-poppler zathura-cb xclip maim renameutils \
-    ffmpegthumbnailer xdotool pkg-config
+    ffmpegthumbnailer xdotool pkg-config dmenu
 
 doas install -Dm 655 "$here/system/dwm_run" \
     "/usr/local/bin/"
@@ -31,12 +31,10 @@ git clone "https://github.com/tonijarjour/st" "$HOME/suckless/st"
 cd "$HOME/suckless/st" || return
 doas make clean install
 
-git clone "git://git.suckless.org/dmenu" "$HOME/suckless/dmenu"
-cd "$HOME/suckless/dmenu" || return
-doas make clean install
-
 git clone "git://git.suckless.org/tabbed" "$HOME/suckless/tabbed"
 cd "$HOME/suckless/tabbed" || return
+curl https://tools.suckless.org/tabbed/patches/autohide/tabbed-autohide-20201222-dabf6a2.diff > autohide.diff
+patch < autohide.diff
 doas make clean install
 
 git clone "https://github.com/nsxiv/nsxiv" "$HOME/suckless/nsxiv"
