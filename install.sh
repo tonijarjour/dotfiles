e9cd917a78b64dcc1a85168d5389bfe5579f87b5
Toni Jarjour <39845673+tonijarjour@users.noreply.github.com>
2021-11-11 20:43:40 -0500

    initial rust and lunar vim setup

--- install.sh
+++ install.sh
@@ -52,6 +52,8 @@ doas make clean install
 # fix quitoncd, nsxiv.desktop, and tabbed nsxiv call with -a
 mkdir -p "$HOME/.config/nnn/plugins"
 curl -Ls https://raw.githubusercontent.com/jarun/nnn/master/plugins/getplugs | sh
+curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
+bash <(curl -s https://raw.githubusercontent.com/lunarvim/lunarvim/master/utils/installer/install.sh)
 
 ln -sf "$here/home/."* "$HOME/"
 ln -sf "$here/config/"* "$HOME/.config/"
