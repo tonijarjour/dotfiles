8f82cf253d8fc246a0cc0191f21691ed2f3afa37
Toni Jarjour <39845673+tonijarjour@users.noreply.github.com>
2022-08-15 23:32:21 -0400

    switch to dwl

--- install.sh
+++ install.sh
@@ -6,10 +6,10 @@ here="$PWD"
 
 doas ln -sf "/run/systemd/resolve/stub-resolv.conf" "/etc/resolv.conf"
 
-doas pacman -S base-devel man-db git neovim fd ripgrep alacritty mpv npm \
+doas pacman -S base-devel man-db fd ripgrep alacritty mpv npm wlroots libinput \
     pkgstats bemenu-wayland swaybg zathura-pdf-mupdf zathura-cb wl-clipboard \
     ttf-iosevka-nerd ttf-liberation noto-fonts noto-fonts-cjk noto-fonts-emoji \
-    imv slurp grim thunar tumbler pipewire-pulse
+    imv slurp grim pipewire-pulse
 
 git clone "https://aur.archlinux.org/nvim-packer-git.git" "$HOME/packer"
 cd "$HOME/packer" || exit 1
@@ -20,14 +20,16 @@ cd "$HOME/chrome" || exit 1
 makepkg -si
 
 git clone "https://github.com/djpohly/dwl.git" "$HOME/dwl"
-curl --create-dirs --output-dir "$HOME/dwl/patches" --remote-name-all "https://github.com/djpohly/dwl/compare/main...{guidocella:alwayscenter.patch,AurelWeinhold:attachbottom.patch}"
-cp "$here/dwl-config.patch" "$HOME/dwl/patches"
-cp "$HOME/dwl/config.def.h" "$HOME/dwl/config.h"
+curl --create-dirs --output-dir "$HOME/dwl/patches" --remote-name-all \
+  "https://github.com/djpohly/dwl/compare/main...{guidocella:alwayscenter.patch,AurelWeinhold:attachbottom.patch}"
+cp "$here/dwl-config.h" "$HOME/dwl/config.h"
 cd "$HOME/dwl" || exit 1
 for p in "$HOME/dwl/patches/"*.patch;
   do patch < $p
 done
 doas make clean install
+
+doas install -Dm 655 "$here/dwl_run" "/usr/local/bin/"
 
 doas usermod -aG seat $USER
 doas systemctl enable seatd.service
