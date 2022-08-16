2fa02f2f4f3d6e1bd98f4b5d3f91f657c1f3c3fe
Toni Jarjour <39845673+tonijarjour@users.noreply.github.com>
2022-08-16 13:26:18 -0400

    back to xorg

--- install.sh
+++ install.sh
@@ -6,33 +6,21 @@ here="$PWD"
 
 doas ln -sf "/run/systemd/resolve/stub-resolv.conf" "/etc/resolv.conf"
 
-doas pacman -S base-devel man-db fd ripgrep alacritty mpv npm wlroots libinput \
-    pkgstats bemenu-wayland swaybg zathura-pdf-mupdf zathura-cb wl-clipboard \
+doas pacman -S base-devel man-db fd ripgrep neovim alacritty mpv npm maim feh \
     ttf-iosevka-nerd ttf-liberation noto-fonts noto-fonts-cjk noto-fonts-emoji \
-    imv slurp grim pipewire-pulse
+    pkgstats dmenu zathura-pdf-mupdf zathura-cb xclip pipewire-pulse xorg-xinit
 
 git clone "https://aur.archlinux.org/nvim-packer-git.git" "$HOME/packer"
 cd "$HOME/packer" || exit 1
 makepkg -si
 
+git clone "https://aur.archlinux.org/nsxiv.git" "$HOME/nsxiv"
+cd "$HOME/nsxiv" || exit 1
+makepkg -si
+
 git clone "https://aur.archlinux.org/google-chrome.git" "$HOME/chrome"
 cd "$HOME/chrome" || exit 1
 makepkg -si
-
-git clone "https://github.com/djpohly/dwl.git" "$HOME/dwl"
-curl --create-dirs --output-dir "$HOME/dwl/patches" --remote-name-all \
-  "https://github.com/djpohly/dwl/compare/main...{guidocella:alwayscenter.patch,AurelWeinhold:attachbottom.patch}"
-cp "$here/dwl-config.h" "$HOME/dwl/config.h"
-cd "$HOME/dwl" || exit 1
-for p in "$HOME/dwl/patches/"*.patch;
-  do patch < $p
-done
-doas make clean install
-
-doas install -Dm 655 "$here/dwl_run" "/usr/local/bin/"
-
-doas usermod -aG seat $USER
-doas systemctl enable seatd.service
 
 curl --proto '=https' --tlsv1.2 -sSf "https://sh.rustup.rs" | sh
 
