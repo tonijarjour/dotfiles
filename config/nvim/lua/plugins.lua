898f47e87efe4d44baadbee375f9d765c1c4b947
Toni Jarjour <39845673+tonijarjour@users.noreply.github.com>
2022-09-24 17:52:02 -0400

    replace nvim with helix

--- config/nvim/lua/plugins.lua
+++
@@ -1,8 +0,0 @@
-return require('packer').startup(function()
-    use {"catppuccin/nvim", as = "catppuccin"}
-    use 'windwp/nvim-autopairs'
-    use {'nvim-lualine/lualine.nvim',
-        requires = {'kyazdani42/nvim-web-devicons', opt = true}}
-    use {'akinsho/bufferline.nvim', requires = 'kyazdani42/nvim-web-devicons'}
-    use {'nvim-treesitter/nvim-treesitter', run = ':TSUpdate'}
-end)
