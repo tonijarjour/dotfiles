6bd05946831bd851d0376c4c00733303a5f2cfb0
Toni Jarjour <39845673+tonijarjour@users.noreply.github.com>
2023-03-05 09:17:45 -0500

    one command per s

--- install.sh
+++ install.sh
@@ -13,19 +13,19 @@ s() {
   su -c bash -c "$C"
 }
 
-git clone "https://github.com/tonijarjour/dwm.git" "$HOME/dwm"
-ln -s "$here/dwm.h" "$HOME/dwm/config.h"
+s ln -sf "/run/systemd/resolve/stub-resolv.conf" "/etc/resolv.conf"
+s install -Dm 644 "$here/50-mouse-acceleration.conf" "/etc/X11/xorg.conf.d/"
+s install -Dm 644 "$here/arabic.conf" "/etc/fonts/local.conf"
 
-s \
-ln -sf "/run/systemd/resolve/stub-resolv.conf" "/etc/resolv.conf";\
-pacman -S man-db fd ripgrep neovim alacritty mpv maim feh sxiv pkgstats \
+s pacman -S man-db fd ripgrep neovim alacritty mpv maim feh sxiv pkgstats \
   ttf-iosevka-nerd ttf-liberation noto-fonts noto-fonts-cjk noto-fonts-emoji \
   xorg-server xorg-xinit xorg-xsetroot dmenu zathura-pdf-mupdf zathura-cb \
-  pipewire-pulse pipewire-jack xclip redshift chromium arc-solid-gtk-theme;\
-install -Dm 644 "$here/50-mouse-acceleration.conf" "/etc/X11/xorg.conf.d/";\
-install -Dm 644 "$here/arabic.conf" "/etc/fonts/local.conf";\
-cd "$HOME/dwm" || exit 1;\
-make clean install
+  pipewire-pulse pipewire-jack xclip redshift chromium arc-solid-gtk-theme
+
+git clone "https://github.com/tonijarjour/dwm.git" "$HOME/dwm"
+ln -s "$here/dwm.h" "$HOME/dwm/config.h"
+cd "$HOME/dwm" || exit 1
+s make clean install
 
 git clone "https://aur.archlinux.org/nvim-packer-git.git" "$HOME/packer"
 cd "$HOME/packer" || exit 1
