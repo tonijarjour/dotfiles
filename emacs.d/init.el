e5dbd3d07a4a4291e77220c2b04186df4d884463
Toni Jarjour <39845673+tonijarjour@users.noreply.github.com>
2023-03-17 09:20:27 -0400

    remove custom set vars for now

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -112,16 +112,3 @@
   "f e"   'counsel-M-x
   "f n"   'next-buffer
   "f p"   'previous-buffer)
-
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(package-selected-packages '(counsel ivy-rich use-package)))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
