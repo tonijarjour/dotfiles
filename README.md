0df86d48ac72a7d31e811e7729427a507f51db07
Toni Jarjour <39845673+tonijarjour@users.noreply.github.com>
2023-03-22 15:28:07 -0400

    remove emacs stuff

--- README.md
+++
@@ -1,9 +0,0 @@
-## Post-Installation
-
-Add email to `config.el`
-
-```shell
-nvm i node
-npm i -g typescript-language-server typescript js-beautify
-npm i -g stylelint markdownlint textlint marked
-```
--- install.sh
+++ install.sh
@@ -15,8 +15,7 @@ prompt \
   ttf-iosevka-nerd ttf-croscore noto-fonts noto-fonts-cjk noto-fonts-emoji \
   xorg-server xorg-xinit xorg-xsetroot zathura-pdf-mupdf zathura-cb redshift \
   pipewire-pulse pipewire-jack arc-solid-gtk-theme man-db texinfo fakeroot \
-  gcc autoconf automake pkgconf make patch imagemagick yt-dlp bacon emacs \
-  texlive-core texlab enchant hunspell hunspell-en_us languagetool tidy"
+  gcc autoconf automake pkgconf make patch imagemagick yt-dlp bacon"
 
 git clone "https://github.com/tonijarjour/dwm.git" "$HOME/dwm"
 ln -s "$here/system/dwm.h" "$HOME/dwm/config.h"
@@ -40,15 +39,9 @@ echo "Reject Noto Nastaliq Urdu font"
 prompt "install -Dm 644 \"$here/system/arabic.conf\" \"/etc/fonts/conf.d/66-noto-reject-nastaliq.conf\""
 
 curl --proto "=https" --tlsv1.2 -sSf "https://sh.rustup.rs" | sh
-"$HOME/.cargo/bin/rustup" component add rust-src rust-analyzer
-ln -s "$HOME/.rustup/toolchains/stable-x86_64-unknown-linux-gnu/bin/rust-analyzer" \
-  "$HOME/.cargo/bin/"
 
 mkdir -p "$HOME/.config"
 ln -sf "$here/config/"* "$HOME/.config/"
-
-git clone --depth 1 "https://github.com/doomemacs/doomemacs" "$HOME/.config/emacs"
-"$HOME/.config/emacs/bin/doom" install --no-config --no-env
 
 for f in "$here/home/"*
 do
