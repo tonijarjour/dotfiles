db9fd20433634a61b8bc7835ab073480b18de9d4
Toni Jarjour <39845673+tonijarjour@users.noreply.github.com>
2023-09-07 18:14:48 -0400

    install xrandr; rust-analyzer link not needed

--- install.sh
+++ install.sh
@@ -15,7 +15,7 @@ prompt \
   ttf-iosevka-nerd ttf-croscore noto-fonts noto-fonts-cjk noto-fonts-emoji \
   xorg-server xorg-xinit xorg-xsetroot zathura-pdf-mupdf zathura-cb redshift \
   pipewire-pulse pipewire-jack arc-solid-gtk-theme man-db texinfo fakeroot \
-  gcc autoconf automake pkgconf make patch bacon nsxiv"
+  gcc autoconf automake pkgconf make patch bacon nsxiv xorg-xrandr"
 
 git clone "https://github.com/tonijarjour/dwm.git" "$HOME/dwm"
 ln -s "$here/system/dwm.h" "$HOME/dwm/config.h"
@@ -39,8 +39,7 @@ echo "Reject Noto Nastaliq Urdu font"
 prompt "install -Dm 644 \"$here/system/arabic.conf\" \"/etc/fonts/conf.d/66-noto-reject-nastaliq.conf\""
 
 curl --proto "=https" --tlsv1.2 -sSf "https://sh.rustup.rs" | sh
-rustup component add rust-src rust-analyzer
-ln -s "$HOME/.rustup/toolchains/stable-x86_64-unknown-linux-gnu/bin/rust-analyzer" "$HOME/.cargo/bin"
+rustup component add rust-src
 
 mkdir -p "$HOME/.config"
 ln -sf "$here/config/"* "$HOME/.config/"
